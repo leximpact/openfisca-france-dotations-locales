2c8c4c5269ec96dde82e31877c6ac473ac098c57
sandcha <sandcha@users.noreply.github.com>
2020-06-11 15:41:12 +0200

    Add input data and test it briefly

---
+++ openfisca_france_dotations_locales/data/REFERENCES.md
@@ -0,0 +1,3 @@
+## Sources des données
+
+* [2019-communes-criteres-repartition.csv](https://www.data.gouv.fr/fr/datasets/criteres-de-repartition-des-dotations-versees-par-letat-aux-collectivites-territoriales/)
---
+++ openfisca_france_dotations_locales/tests/test_data.py
@@ -0,0 +1,22 @@
+
+import pandas
+
+
+def test_csv_communes_criteres_repartition():
+    csv_communes_criteres_repartition = './openfisca_france_dotations_locales/data/2019-communes-criteres-repartition.csv'
+    
+    communes_criteres_repartition_2019 = pandas.read_csv(
+        csv_communes_criteres_repartition,
+        decimal=",")
+
+    assert len(communes_criteres_repartition_2019["Informations générales - Nom de la commune"]) == 35056
+
+    # nombre_habitants = communes_criteres_repartition_2019[
+    #     "Informations générales - Population DGF Année N'"
+    #     ]
+    # pfi_habitant = communes_criteres_repartition_2019[
+    #     "Potentiel fiscal et financier des communes - Potentiel financier par habitant"
+    #     ]
+    # codes_insee = communes_criteres_repartition_2019[
+    #     "Informations générales - Code INSEE de la commune"
+    # ]
