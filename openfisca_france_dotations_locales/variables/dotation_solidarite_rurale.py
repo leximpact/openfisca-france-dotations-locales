6d4ece819e1ad650078224b3aad8777b93b983b1
Augustin Wenger <magemax@yahoo.com>
2020-06-18 15:38:05 +0200

    Ajoute le calcul d'éligibilité de la dsr fraction bourg-centre

--- openfisca_france_dotations_locales/variables/dotation_solidarite_rurale.py
+++ openfisca_france_dotations_locales/variables/dotation_solidarite_rurale.py
@@ -1,5 +1,6 @@
 from openfisca_core.model_api import *
 from openfisca_france_dotations_locales.entities import *
+import numpy as np
 
 
 class dotation_solidarite_rurale(Variable):
@@ -40,8 +41,80 @@ class dsr_eligible_fraction_perequation(Variable):
         potentiel_financier_par_habitant_strate = commune('potentiel_financier_par_habitant_moyen', period)
 
         plafond = 2 * potentiel_financier_par_habitant_strate
-        outre_mer =commune('outre_mer', period)
+        outre_mer = commune('outre_mer', period)
         return (~outre_mer) * (population_dgf < seuil_nombre_habitants) * (potentiel_financier_par_habitant <= plafond)
+
+
+class dsr_eligible_fraction_bourg_centre_type_1(Variable):
+    value_type = bool
+    entity = Commune
+    definition_period = YEAR
+    label = "eligibilité à la fraction bourg-centre de la DSR pour les \
+        communes de taille inférieure au seuil de 10000 habitants"
+    reference = [
+        'Code général des collectivités territoriales - Article L2334-21',
+        'https://www.legifrance.gouv.fr/affichCodeArticle.do?idArticle=LEGIARTI000033878277&cidTexte=LEGITEXT000006070633',
+        "http://www.dotations-dgcl.interieur.gouv.fr/consultation/documentAffichage.php?id=94"
+        ]
+    documentation = '''
+        La première fraction de la dotation de solidarité rurale est attribuée
+        aux communes de moins de 10000 habitants chefs-lieux de canton, ou bureaux
+        centralisateurs,ou dont la population représente au moins 15% de la
+        population du canton.La qualité de chef-lieu de canton s’apprécie au
+        1er janvier 2014, de même que le périmètre cantonal.
+    '''
+
+    def formula(commune, period, parameters):
+        population_dgf_plafonnee = commune("population_dgf_plafonnee", period)
+        bureau_centralisateur = commune("bureau_centralisateur", period)
+        chef_lieu_de_canton = commune("chef_lieu_de_canton", period)
+        part_population_canton = commune("part_population_canton", period)
+        seuil_part_population_canton = parameters(period).dotation_solidarite_rurale.bourg_centre.eligibilite.seuil_part_population_canton
+        outre_mer = commune('outre_mer', period)
+        population_dgf_agglomeration = commune("population_dgf_agglomeration", period)
+        population_dgf_departement_agglomeration = commune("population_dgf_departement_agglomeration", period)
+        population_dgf_maximum_commune_agglomeration = commune("population_dgf_maximum_commune_agglomeration", period)
+        chef_lieu_departement_dans_agglomeration = commune("chef_lieu_departement_dans_agglomeration", period)
+        population_dgf_chef_lieu_de_canton = commune("population_dgf_chef_lieu_de_canton", period)
+        potentiel_financier = commune('potentiel_financier', period)
+        population_dgf = commune('population_dgf', period)
+        potentiel_financier_par_habitant = commune('potentiel_financier_par_habitant', period)
+
+        taille_max_commune = parameters(period).dotation_solidarite_rurale.seuil_nombre_habitants
+        taille_eligible = (population_dgf_plafonnee < taille_max_commune)
+
+        preeligible = (~outre_mer) * taille_eligible * ((part_population_canton >= seuil_part_population_canton) | bureau_centralisateur | chef_lieu_de_canton)
+
+        # Restent des sources d'exclusion de l'éligibilité...
+        # Situées dans une agglomération :
+        # 1° a) Représentant au moins 10 % de la population du département
+        # b) Comptant une commune soit de plus de 100 000 habitants, soit chef-lieu de département ;
+        dsr_bc_part_max_pop_departement = parameters(period).dotation_solidarite_rurale.bourg_centre.eligibilite.exclusion.seuil_part_population_dgf_departement_agglomeration
+        dsr_bc_pop_max_agglo = parameters(period).dotation_solidarite_rurale.bourg_centre.eligibilite.exclusion.seuil_population_dgf_agglomeration
+        dsr_bc_taille_max_plus_grande_commune_agglo = parameters(period).dotation_solidarite_rurale.bourg_centre.eligibilite.exclusion.seuil_population_dgf_maximum_commune_agglomeration
+
+        eligible = (preeligible
+            * (population_dgf_agglomeration < min_(dsr_bc_pop_max_agglo, dsr_bc_part_max_pop_departement * population_dgf_departement_agglomeration))
+            * (population_dgf_maximum_commune_agglomeration < dsr_bc_taille_max_plus_grande_commune_agglo)
+            * (~chef_lieu_departement_dans_agglomeration)
+            )
+        # 2° Situées dans un canton dont la commune chef-lieu compte plus de 10 000 habitants,
+        # à l'exception des communes sièges des bureaux centralisateurs ;
+        dsr_bc_taille_max_chef_lieu_canton = parameters(period).dotation_solidarite_rurale.bourg_centre.eligibilite.exclusion.seuil_population_dgf_chef_lieu_de_canton
+        eligible = eligible * ((population_dgf_chef_lieu_de_canton < dsr_bc_taille_max_chef_lieu_canton) | bureau_centralisateur)
+
+        # 4° Dont le potentiel financier par habitant est supérieur au double du potentiel
+        # financier moyen par habitant des communes de moins de 10 000 habitants.
+        dsr_bc_ratio_max_pot_fin = parameters(period).dotation_solidarite_rurale.bourg_centre.eligibilite.exclusion.seuil_rapport_pfi_10000
+
+        # On pourrait en créer une variable, on va l'utiliser 3 fois dans 3 fonctions différentes.
+        # Ou alors un paramètre calculable ?
+        # oui le taille_max_commune est le même que pour le seuil d'éligibilité, notre paramétrisation est ainsi
+        pot_fin_10000 = (np.sum((~outre_mer) * (population_dgf < taille_max_commune) * potentiel_financier)
+                / np.sum((~outre_mer) * (population_dgf < taille_max_commune) * population_dgf))
+        eligible = eligible * (potentiel_financier_par_habitant < (dsr_bc_ratio_max_pot_fin * pot_fin_10000))
+
+        return eligible
 
 
 class dsr_fraction_perequation(Variable):
