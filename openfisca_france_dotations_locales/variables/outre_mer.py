5dc858f6f29cb7e31ccb38504b6ff41e4042beb6
Augustin Wenger <magemax@yahoo.com>
2020-06-18 15:37:03 +0200

    change la période pour l'outre-mer

--- openfisca_france_dotations_locales/variables/outre_mer.py
+++ openfisca_france_dotations_locales/variables/outre_mer.py
@@ -5,5 +5,5 @@ from openfisca_france_dotations_locales.entities import *
 class outre_mer(Variable):
     value_type = bool
     entity = Commune
-    definition_period = YEAR
+    definition_period = ETERNITY
     label = "Caractère ultramarin de la commune"
