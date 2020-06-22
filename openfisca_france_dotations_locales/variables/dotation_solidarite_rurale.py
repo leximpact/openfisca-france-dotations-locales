b882bb80ec74b7f9aa3edcf630c219b71b2c10a8
Augustin Wenger <magemax@yahoo.com>
2020-06-22 14:25:15 +0200

    Calcule l'exclusion de type 2 comme on le fait pour l'exclusion de type 1

--- openfisca_france_dotations_locales/variables/dotation_solidarite_rurale.py
+++ openfisca_france_dotations_locales/variables/dotation_solidarite_rurale.py
@@ -129,6 +129,22 @@ class dsr_exclue_fraction_bourg_centre_type_1(Variable):
             | dsr_exclue_fraction_bourg_centre_pfi)
 
 
+class dsr_exclue_fraction_bourg_centre_type_2(Variable):
+    value_type = bool
+    entity = Commune
+    definition_period = YEAR
+    label = "Exclusion du bénéfice de la fraction bourg-centre de la DSR pour les \
+        communes de taille inférieure au seuil de 10 000 habitants"
+
+    def formula(commune, period, parameters):
+        # Sources d'exclusion de l'éligibilité...
+        dsr_exclue_fraction_bourg_centre_agglomeration = commune("dsr_exclue_fraction_bourg_centre_agglomeration", period)
+        dsr_exclue_fraction_bourg_centre_pfi = commune("dsr_exclue_fraction_bourg_centre_pfi", period)
+
+        return (dsr_exclue_fraction_bourg_centre_agglomeration
+            | dsr_exclue_fraction_bourg_centre_pfi)
+
+
 class dsr_eligible_fraction_bourg_centre_type_1(Variable):
     value_type = bool
     entity = Commune
@@ -168,6 +184,7 @@ class dsr_eligible_fraction_bourg_centre_type_1(Variable):
 
         return preeligible * not_(dsr_exclue_fraction_bourg_centre_type_1)
 
+
 class dsr_eligible_fraction_bourg_centre_type_2(Variable):
     value_type = bool
     entity = Commune
@@ -188,47 +205,16 @@ class dsr_eligible_fraction_bourg_centre_type_2(Variable):
     def formula(commune, period, parameters):
         population_dgf_plafonnee = commune("population_dgf_plafonnee", period)
         outre_mer = commune('outre_mer', period)
-        population_dgf_agglomeration = commune("population_dgf_agglomeration", period)
-        population_dgf_departement_agglomeration = commune("population_dgf_departement_agglomeration", period)
-        population_dgf_maximum_commune_agglomeration = commune("population_dgf_maximum_commune_agglomeration", period)
-        chef_lieu_departement_dans_agglomeration = commune("chef_lieu_departement_dans_agglomeration", period)
-        potentiel_financier = commune('potentiel_financier', period)
-        population_dgf = commune('population_dgf', period)
-        potentiel_financier_par_habitant = commune('potentiel_financier_par_habitant', period)
         chef_lieu_arrondissement = commune("chef_lieu_arrondissement", period)
+        dsr_exclue_fraction_bourg_centre_type_2 = commune('dsr_exclue_fraction_bourg_centre_type_2', period)
 
         taille_max_commune = parameters(period).dotation_solidarite_rurale.seuil_nombre_habitants
         taille_max_chef_lieu_arrondissement = parameters(period).dotation_solidarite_rurale.bourg_centre.eligibilite.seuil_nombre_habitants_chef_lieu
-
         taille_eligible = (population_dgf_plafonnee >= taille_max_commune) * (population_dgf_plafonnee < taille_max_chef_lieu_arrondissement)
 
         preeligible = (~outre_mer) * taille_eligible * chef_lieu_arrondissement
 
-        # Restent des sources d'exclusion de l'éligibilité...
-        # Situées dans une agglomération :
-        # 1° a) Représentant au moins 10 % de la population du département
-        # b) Comptant une commune soit de plus de 100 000 habitants, soit chef-lieu de département ;
-        dsr_bc_part_max_pop_departement = parameters(period).dotation_solidarite_rurale.bourg_centre.eligibilite.exclusion.seuil_part_population_dgf_agglomeration_departement
-        dsr_bc_pop_max_agglo = parameters(period).dotation_solidarite_rurale.bourg_centre.eligibilite.exclusion.seuil_population_dgf_agglomeration
-        dsr_bc_taille_max_plus_grande_commune_agglo = parameters(period).dotation_solidarite_rurale.bourg_centre.eligibilite.exclusion.seuil_population_dgf_maximum_commune_agglomeration
-
-        eligible = (preeligible
-            * (population_dgf_agglomeration < min_(dsr_bc_pop_max_agglo, dsr_bc_part_max_pop_departement * population_dgf_departement_agglomeration))
-            * (population_dgf_maximum_commune_agglomeration < dsr_bc_taille_max_plus_grande_commune_agglo)
-            * (~chef_lieu_departement_dans_agglomeration)
-            )
-        # 4° Dont le potentiel financier par habitant est supérieur au double du potentiel
-        # financier moyen par habitant des communes de moins de 10 000 habitants.
-        dsr_bc_ratio_max_pot_fin = parameters(period).dotation_solidarite_rurale.bourg_centre.eligibilite.exclusion.seuil_rapport_pfi_10000
-
-        # On pourrait en créer une variable, on va l'utiliser 3 fois dans 3 fonctions différentes.
-        # Ou alors un paramètre calculable ?
-        # oui le taille_max_commune est le même que pour le seuil d'éligibilité, notre paramétrisation est ainsi
-        pot_fin_10000 = (np.sum((~outre_mer) * (population_dgf < taille_max_commune) * potentiel_financier)
-                / np.sum((~outre_mer) * (population_dgf < taille_max_commune) * population_dgf))
-        eligible = eligible * (potentiel_financier_par_habitant < (dsr_bc_ratio_max_pot_fin * pot_fin_10000))
-
-        return eligible
+        return preeligible * not_(dsr_exclue_fraction_bourg_centre_type_2)
 
 
 class dsr_eligible_fraction_bourg_centre(Variable):
