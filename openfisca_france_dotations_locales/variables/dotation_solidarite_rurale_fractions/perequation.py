2b096863e887cb2d792de17e6077d14837c60d95
Augustin Wenger <magemax@yahoo.com>
2020-07-07 12:55:43 +0200

    Ajout du montant total attribué pour la DSR fraction péréquation et sa répartition; tests

--- openfisca_france_dotations_locales/variables/dotation_solidarite_rurale_fractions/perequation.py
+++ openfisca_france_dotations_locales/variables/dotation_solidarite_rurale_fractions/perequation.py
@@ -27,6 +27,75 @@ class dsr_eligible_fraction_perequation(Variable):
         return (~outre_mer) * (population_dgf < seuil_nombre_habitants) * (potentiel_financier_par_habitant <= plafond)
 
 
+class dsr_montant_total_eligibles_fraction_perequation(Variable):
+    value_type = float
+    entity = Commune
+    definition_period = YEAR
+    label = "Montant disponible pour communes éligibles DSR fraction bourg-centre"
+    reference = "http://www.dotations-dgcl.interieur.gouv.fr/consultation/documentAffichage.php?id=94"
+
+    def formula(commune, period, parameters):
+        montant_total_a_attribuer = 645_050_872 - 7_403_123
+        # montant inscrit dans la note. Pour le transformer en formule il faut
+        # que soient implémentés :
+        # les formules de garanties pour communes nouvellement non éligibles (moyen)
+        # les garanties communes nouvelles (chaud)
+        # la répartition du montant global vers la DSR (très difficile)
+        return montant_total_a_attribuer
+
+
+class dsr_montant_total_eligibles_fraction_perequation_part_potentiel_financier_par_habitant(Variable):
+    value_type = float
+    entity = Commune
+    definition_period = YEAR
+    label = "Montant total DSR fraction péréquation - potentiel financier par habitant:\
+Valeur totale attribuée (hors garanties de stabilité) aux communes éligibles à la fraction péréquation de la DSR au titre du potentiel financier par habitant"
+    reference = "https://www.legifrance.gouv.fr/affichCodeArticle.do?idArticle=LEGIARTI000036433094&cidTexte=LEGITEXT000006070633"
+
+    def formula(commune, period, parameters):
+        poids = parameters(period).dotation_solidarite_rurale.attribution.poids_potentiel_financier_par_habitant
+        return commune('dsr_montant_total_eligibles_fraction_perequation', period) * poids
+
+
+class dsr_montant_total_eligibles_fraction_perequation_part_longueur_voirie(Variable):
+    value_type = float
+    entity = Commune
+    definition_period = YEAR
+    label = "Montant total DSR fraction péréquation - longueur voirie:\
+Valeur totale attribuée (hors garanties de stabilité) aux communes éligibles à la fraction péréquation de la DSR au titre de la longueur de voirie"
+    reference = "https://www.legifrance.gouv.fr/affichCodeArticle.do?idArticle=LEGIARTI000036433094&cidTexte=LEGITEXT000006070633"
+
+    def formula(commune, period, parameters):
+        poids = parameters(period).dotation_solidarite_rurale.attribution.poids_longueur_voirie
+        return commune('dsr_montant_total_eligibles_fraction_perequation', period) * poids
+
+
+class dsr_montant_total_eligibles_fraction_perequation_part_enfants(Variable):
+    value_type = float
+    entity = Commune
+    definition_period = YEAR
+    label = "Montant total DSR fraction péréquation - nombre d'enfants:\
+Valeur totale attribuée (hors garanties de stabilité) aux communes éligibles à la fraction péréquation de la DSR au titre du nombre d'enfants"
+    reference = "https://www.legifrance.gouv.fr/affichCodeArticle.do?idArticle=LEGIARTI000036433094&cidTexte=LEGITEXT000006070633"
+
+    def formula(commune, period, parameters):
+        poids = parameters(period).dotation_solidarite_rurale.attribution.poids_enfants
+        return commune('dsr_montant_total_eligibles_fraction_perequation', period) * poids
+
+
+class dsr_montant_total_eligibles_fraction_perequation_part_potentiel_financier_par_hectare(Variable):
+    value_type = float
+    entity = Commune
+    definition_period = YEAR
+    label = "Montant total DSR fraction péréquation - potentiel financier par hectare:\
+Valeur totale attribuée (hors garanties de stabilité) aux communes éligibles à la fraction péréquation de la DSR au titre du potentiel financier par hectare"
+    reference = "https://www.legifrance.gouv.fr/affichCodeArticle.do?idArticle=LEGIARTI000036433094&cidTexte=LEGITEXT000006070633"
+
+    def formula(commune, period, parameters):
+        poids = parameters(period).dotation_solidarite_rurale.attribution.poids_potentiel_financier_par_hectare
+        return commune('dsr_montant_total_eligibles_fraction_perequation', period) * poids
+
+
 class dsr_fraction_perequation(Variable):
     value_type = float
     entity = Commune
