4f375c3e4e7eeaaa1a73396ad51b0027807be7f0
sandcha <sandcha@users.noreply.github.com>
2020-07-13 18:38:43 +0200

    fixup! Ajouts mineurs d'unités et documentation

--- openfisca_france_dotations_locales/variables/insulaire.py
+++ openfisca_france_dotations_locales/variables/insulaire.py
@@ -7,10 +7,10 @@ class insulaire(Variable):
     entity = Commune
     definition_period = YEAR
     label = "Commune insulaire:\
-Commune insulaire au sens de l'article 2334-22 du CGCL"
+        Commune insulaire au sens de l'article 2334-22 du CGCL"
     documentation = """
-    une commune insulaire s'entend d'une commune de métropole située sur une île qui,
+    Une commune insulaire s'entend d'une commune de métropole située sur une île qui,
     n'étant pas reliée au continent par une infrastructure routière, comprend une
-    seule commune ou un seul établissement public de coopération intercommunale
+    seule commune ou un seul établissement public de coopération intercommunale.
     """
     reference = "https://www.legifrance.gouv.fr/affichCodeArticle.do?idArticle=LEGIARTI000036433094&cidTexte=LEGITEXT000006070633"
