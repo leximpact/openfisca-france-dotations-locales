5e93a0aa7d3bf77357ebc52f4d7cd865b797ee81
Augustin Wenger <magemax@yahoo.com>
2020-08-13 18:56:51 +0200

    Calcul du montant global (incluant toutes les fractions) versé à chaque commune au titre de la dotation de solidarité rurale

--- openfisca_france_dotations_locales/variables/dotation_solidarite_rurale.py
+++ openfisca_france_dotations_locales/variables/dotation_solidarite_rurale.py
@@ -8,3 +8,9 @@ class dotation_solidarite_rurale(Variable):
     definition_period = YEAR
     label = "Dotation de solidarité rurale (DSR)"
     reference = "http://www.dotations-dgcl.interieur.gouv.fr/consultation/documentAffichage.php?id=94"
+
+    def formula(commune, period, parameters):
+        dsr_fraction_cible = commune("dsr_fraction_cible", period)
+        dsr_fraction_bourg_centre = commune("dsr_fraction_bourg_centre", period)
+        dsr_fraction_perequation = commune("dsr_fraction_perequation", period)
+        return dsr_fraction_cible + dsr_fraction_bourg_centre + dsr_fraction_perequation
