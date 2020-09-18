d0b3c0d29f9079b0e7234fd7c8bd66fdf0aef0a6
sandcha <sandcha@users.noreply.github.com>
2020-09-18 14:45:04 +0200

    Ajout de la part_compensations_part_salaires

--- openfisca_france_dotations_locales/variables/dotation_forfaitaire.py
+++ openfisca_france_dotations_locales/variables/dotation_forfaitaire.py
@@ -8,3 +8,16 @@ class dotation_forfaitaire(Variable):
     definition_period = YEAR
     label = "Dotation forfaitaire (DF)"
     reference = "https://www.legifrance.gouv.fr/codes/section_lc/LEGITEXT000006070633/LEGISCTA000006192290?etatTexte=VIGUEUR&etatTexte=VIGUEUR_DIFF#LEGISCTA000006192290"
+
+    def formula_2018(commune, period, parameters):
+        dotation_forfaitaire_an_dernier = commune('dotation_forfaitaire', period.offset(1, 'year'))
+        part_compensations_part_salaires = commune('part_compensations_part_salaires', period)
+        return dotation_forfaitaire_an_dernier + part_compensations_part_salaires
+
+
+class part_compensations_part_salaires(Variable):
+    value_type = float
+    entity = Commune
+    definition_period = YEAR
+    label = "Part de compensations part salaires (CPS) transférée aux communes"
+    reference = "https://www.collectivites-locales.gouv.fr/files/files/dgcl_v2/FLAE_circulaires_10_fevrier2016/note_dinformation_2019_dfcom_-_vdef2.pdf"
