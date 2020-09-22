2b6f6ba40bbabe5a789dc537964bbed3e8e151a2
Augustin Wenger <magemax@yahoo.com>
2020-09-22 10:54:16 +0200

    Retrait de la part compensation part salaire

--- openfisca_france_dotations_locales/variables/dotation_forfaitaire.py
+++ openfisca_france_dotations_locales/variables/dotation_forfaitaire.py
@@ -11,17 +11,7 @@ class dotation_forfaitaire(Variable):
 
     def formula_2018(commune, period, parameters):
         dotation_forfaitaire_an_dernier = commune('dotation_forfaitaire', period.offset(1, 'year'))
-        part_compensations_part_salaires = commune('part_compensations_part_salaires', period)
-        return dotation_forfaitaire_an_dernier + part_compensations_part_salaires
-
-
-class part_compensations_part_salaires(Variable):
-    value_type = float
-    entity = Commune
-    definition_period = YEAR
-    label = "Retraitement CPS: \
-        retraitement des compensations part salaires (CPS) transférées aux communes (dite part CPS)"
-    reference = "https://www.collectivites-locales.gouv.fr/files/files/dgcl_v2/FLAE_circulaires_10_fevrier2016/note_dinformation_2019_dfcom_-_vdef2.pdf"
+        return dotation_forfaitaire_an_dernier
 
 
 class df_eligible_ecretement(Variable):
