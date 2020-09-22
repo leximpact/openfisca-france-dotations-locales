0b2bf9c5507261e6c447b7a036ecf309cb9109e7
Augustin Wenger <magemax@yahoo.com>
2020-09-22 15:13:27 +0200

    Associe le potentiel fiscal moyen à l'entité correcte

--- openfisca_france_dotations_locales/variables/potentiel_fiscal.py
+++ openfisca_france_dotations_locales/variables/potentiel_fiscal.py
@@ -13,16 +13,19 @@ class potentiel_fiscal(Variable):
 
 class potentiel_fiscal_moyen_national(Variable):
     value_type = int
-    entity = Commune
+    entity = Etat
     definition_period = YEAR
     label = "Potentiel fiscal par habitant moyen national:\
     potentiel fiscal moyen national utilisé pour le calcul de la Dotation forfaitaire. Attention,\
     c'est pas du tout une moyenne (il y a un facteur logarithmé)"
     reference = ["https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000037994287",
         "http://www.dotations-dgcl.interieur.gouv.fr/consultation/documentAffichage.php?id=115"]
-    def formula_2018(commune, period, parameters):
+
+    def formula_2018(etat, period, parameters):
         return 624.197
-    def formula_2019(commune, period, parameters):
+
+    def formula_2019(etat, period, parameters):
         return 631.5677
-    def formula_2020(commune, period, parameters):
+
+    def formula_2020(etat, period, parameters):
         return 641.164387
